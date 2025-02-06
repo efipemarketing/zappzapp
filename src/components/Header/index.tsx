2a0c67003cf9f906d098fb972ce1a062671fc2fd
Felipe Marketing <95032242+efipemarketing@users.noreply.github.com>
2025-02-06 20:39:11 -0300

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,8 +6,8 @@ export default function Header(){
 	return (
 		<header id="header" className="container">
 			<div>
-				<h1>AssisZapp</h1>
-				<p className="color-medium mb-5">SEU ASSISTENTE FINANCEIRO DISPONÍVEL  24H POR DIA NO WHATSAPP</p>
+				<h1>🚀 AssisZapp</h1>
+				<p className="color-medium mb-5">SEU ASSISTENTE FINANCEIRO DISPONÍVEL  24H POR DIA NO WHATSAPP 🤖</p>
 
 				<div className="buttons">
 					<button className="w-100" onClick={() => scrollTo("about-us", -66)}>Saber mais</button>
